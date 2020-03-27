a2d8beb2bb2bd83f0264bc3391549edebe5a4d87
Lateef-Kdk <lateefkdk93@gmail.com>
2020-03-26 23:44:25 -0400

    Im Done

--- app/src/main/java/com/example/theguessinggame/GuessTheNumber.kt
+++ app/src/main/java/com/example/theguessinggame/GuessTheNumber.kt
@@ -1,8 +1,10 @@
 package com.example.theguessinggame
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -12,6 +14,7 @@ class GuessTheNumber : AppCompatActivity() {
     private lateinit var guessingTextView: TextView
     private lateinit var greaterButton: Button
     private lateinit var lessButton: Button
+    private lateinit var count:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,26 +23,43 @@ class GuessTheNumber : AppCompatActivity() {
         guessingTextView = findViewById(R.id.guessingTVID)
         lessButton = findViewById(R.id.lessButtonID)
         greaterButton = findViewById(R.id.greaterButtonID)
+        count = findViewById(R.id.NumCountTVID)
 
-        var begin = 0
-        var end = 1001
+        val pref = getSharedPreferences("MYPREFS",Context.MODE_PRIVATE)
+        findViewById<TextView>(R.id.CorrectTVID).text = ""
+
+        var begin = pref.getInt("LOWERLIMIT",0)
+        var end = pref.getInt("UPPERLIMIT",0)
+        var numGuesses = pref.getString("NUMGUESSES","IDK")
         var i = 0
         var currentNum = (begin + end)/2
+        findViewById<TextView>(R.id.nowIGuessTVID).text = getString(R.string.NowIGuess,numGuesses)
+        count.text = getString(R.string.countit,i)
 
-        guessingTextView.text = getString(R.string.First_Guess)
+        guessingTextView.text = getString(R.string.First_Guess,currentNum)
 
         lessButton.setOnClickListener {
-            Toast.makeText(this,"Less Pressed",Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this,"Less Pressed",Toast.LENGTH_SHORT).show()
             end = currentNum
             currentNum = (begin + end)/2
             guessingTextView.text = getString(R.string.IsItLessThan,currentNum)
+            i++
+            count.text = getString(R.string.countit,i)
+        }
+
+        findViewById<Button>(R.id.neitherBtnID).setOnClickListener{
+            findViewById<TextView>(R.id.CorrectTVID).text = getString(R.string.GotIt,currentNum)
+            i++
+            count.text = getString(R.string.countit,i)
         }
 
         greaterButton.setOnClickListener {
-            Toast.makeText(this,"Less Pressed",Toast.LENGTH_SHORT).show()
+        //    Toast.makeText(this,"Less Pressed",Toast.LENGTH_SHORT).show()
             begin = currentNum
             currentNum = (begin + end)/2
             guessingTextView.text = getString(R.string.IsItGreaterThan,currentNum)
+            i++
+            count.text = getString(R.string.countit,i)
         }
 
         findViewById<Button>(R.id.restardBtnID).setOnClickListener {
--- app/src/main/java/com/example/theguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/theguessinggame/MainActivity.kt
@@ -1,19 +1,89 @@
 package com.example.theguessinggame
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.log10
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var beginningNumber: TextView
+    private lateinit var endingNumber: TextView
+    private lateinit var howManyGuesses: TextView
+    private lateinit var theNumberTheyEntered: TextView
+    var begin = 0
+    var end = 0
+    var numGuesses = 0
+    var difference = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        beginningNumber = findViewById(R.id.beginTV)
+        endingNumber = findViewById(R.id.endTV)
+        theNumberTheyEntered = findViewById(R.id.numberTVID)
+
+//        if(beginningNumber.text.toString() != ""){
+//            begin = beginningNumber.text.toString().toInt()
+//        }
+//        if(endingNumber.text.toString() != ""){
+//            end = endingNumber.text.toString().toInt()
+//        }
+//        difference = begin - end
+//
+//        numGuesses = (log10(difference.toDouble()) / log10(2.0))
+//        Log.i("Calculations",begin.toString())
+//        Log.i("Calculations",end.toString())
+//        Log.i("Calculations",difference.toString())
+//        Log.i("Calculations",numGuesses.toString())
+
+        //create shared preferences
+        val pref = getSharedPreferences("MYPREFS",Context.MODE_PRIVATE)
+        val editor = pref.edit()
+
         findViewById<Button>(R.id.doneBtnID).setOnClickListener {
-            startActivity(Intent(this@MainActivity, GuessTheNumber::class.java))
+            if (beginningNumber.text.toString() == "" || endingNumber.text.toString() == "") {
+                Toast.makeText(this, "Please pick an Upper AND Lower Limit", Toast.LENGTH_SHORT).show()
+            } else if (endingNumber.text.toString().toInt() < beginningNumber.text.toString().toInt()) {
+                Toast.makeText(this, "Upper Limit, can not be less than lower limit", Toast.LENGTH_SHORT).show()
+            } else if (theNumberTheyEntered.text.toString() == "") {
+                Toast.makeText(this, "You have to choose a number!", Toast.LENGTH_SHORT).show()
+            } else if ((theNumberTheyEntered.text.toString().toInt() > endingNumber.text.toString().toInt()) ||
+                (theNumberTheyEntered.text.toString().toInt() < beginningNumber.text.toString().toInt())) {
+                Toast.makeText(this, "That number is out of bounds!", Toast.LENGTH_SHORT).show()
+            } else {
+                begin = beginningNumber.text.toString().toInt()
+                end = endingNumber.text.toString().toInt()
+                difference = end - begin
+
+                //calculating how many guesses it would take to get the right answer
+                numGuesses = (log10(difference.toDouble()) / log10(2.0)).toInt()+1
+
+                Log.i("Calculations", begin.toString())
+                Log.i("Calculations", end.toString())
+                Log.i("Calculations", difference.toString())
+                Log.i("Calculations", numGuesses.toString())
+
+                //save the calculation for the number of guesses
+                editor.putString("NUMGUESSES",numGuesses.toString())
+                //save the the value of the lower limit they choose
+                editor.putInt("LOWERLIMIT",begin)
+                //save the the value of the upper limit they choose
+                editor.putInt("UPPERLIMIT",end)
+                //commit the changes
+                editor.putInt("NUMENTERED",theNumberTheyEntered.text.toString().toInt())
+                Log.i("Calculations", "They Entered "+theNumberTheyEntered.text.toString())
+                editor.apply()
+
+                startActivity(Intent(this@MainActivity, GuessTheNumber::class.java))
+            }
         }
     }
 }
